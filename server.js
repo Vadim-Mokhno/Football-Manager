a327c5278003a934048dd4e3d87de645de0f8445
Vadim-Mokhno <92451101+Vadim-Mokhno@users.noreply.github.com>
2024-07-16 05:41:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -23,3 +23,5 @@ mongoose.connection.once('open', () => {
   console.log('Connected to MongoDB');
   app.listen(PORT, () => console.log('Server is starting on port:' + PORT));
 });
+
+module.exports = app;
