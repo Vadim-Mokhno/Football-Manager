f4d61b109d791e6205aa845b09ba88f09e57a3d2
Vadim-Mokhno <92451101+Vadim-Mokhno@users.noreply.github.com>
2024-07-16 05:03:59 +0300

    Update app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -4,7 +4,33 @@ const request = require('supertest');
 describe('POST', () => {
   it('/products creates a product', async () => {
         expect(2).toEqual(2);
+    
+  });
+});
 
+describe('GET', () => {
+  it('/products creates a product', async () => {
+        expect(2).toEqual(2);
+    
+  });
+});
+
+describe('DELETE', () => {
+  it('/products creates a product', async () => {
+        expect(2).toEqual(2);
+    
+  });
+});
+
+describe('PUT', () => {
+  it('/products creates a product', async () => {
+        expect(2).toEqual(2);
+    
+  });
+});
+describe('PATCH', () => {
+  it('/products creates a product', async () => {
+        expect(2).toEqual(2);
     
   });
 });
