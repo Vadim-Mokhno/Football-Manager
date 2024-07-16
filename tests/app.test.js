fed3456d595abdb375d94010012f8268565f8934
Vadim-Mokhno <92451101+Vadim-Mokhno@users.noreply.github.com>
2024-07-16 05:37:36 +0300

    Update app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -3,20 +3,24 @@ const request = require('supertest');
 
 describe('POST', () => {
   it('creates a product', async () => {
-        expect(2).toEqual(2);
+     const response = await request(app).post('/')
+     expect(2).toEqual(2);
     
   });
 });
 
 describe('GET', () => {
   it('fetches a product', async () => {
-        expect(23).toEqual(23);
+    const response = await request(app).get('/')
+    expect(23).toEqual(23);
     
   });
 });
 
 describe('DELETE', () => {
   it('deletes a product', async () => {
+        const response = await request(app).delete('/1')
+
         expect(233).toEqual(233);
     
   });
@@ -24,12 +28,16 @@ describe('DELETE', () => {
 
 describe('PUT', () => {
   it('creates a product', async () => {
+         const response = await request(app).put('/')
+
         expect(2333).toEqual(2333);
     
   });
 });
 describe('PATCH', () => {
   it('changes a product', async () => {
+         const response = await request(app).patch('/1')
+
         expect(24).toEqual(24);
     
   });
